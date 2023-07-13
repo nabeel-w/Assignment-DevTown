e30c6dd88d79390b46e1e7f686f6d4f5f2dd34af
nabeel-w <hassanfarhat986@gmail.com>
2023-07-14 00:41:32 +0530

    updated fixes

--- tmp.js
+++
@@ -1,7 +0,0 @@
-const { default: mongoose } = require("mongoose");
-
-const username="admin-nabeel"
-const password="N@beel3112"
-const cluster="cluster0.3jl39cv"
-
-mongoose.connect(`mongodb+srv://${username}:${password}@${cluster}.mongodb.net/?retryWrites=true&w=majority`, {useNewUrlParser: true});
